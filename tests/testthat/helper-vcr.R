f358ad53d1a286848d58a9f708d73e2aa56a221a
C. Lente <7017340+clente@users.noreply.github.com>
2022-12-13 17:27:45 -0300

    Mock a token so validate_token() works with vcr

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -3,3 +3,8 @@ invisible(vcr::vcr_configure(
   dir = vcr::vcr_test_path("fixtures")
 ))
 vcr::check_cassette_names()
+
+# Mock the existance of an AOC session to fool validate_token()
+if (Sys.getenv("AOC_SESSION", "") == "") {
+  Sys.setenv("AOC_SESSION" = "VCR_MOCK")
+}
